e6be4119d0892a256b7db5cd616f7b7098dd97f4
twilm <twilm4@tutamail.com>
2022-06-18 15:38:34 +1000

    got the bot searching yellow pages, need to bring it all together in a CSV now for the finished product, tweak a few things and sort out a way to make it easier to use for layman

--- main.py
+++ main.py
@@ -3,6 +3,35 @@ import requests
 import pandas as pd
 
 
+def yellow_pages(company, location):
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.core.utils import ChromeType
+
+    from bs4 import BeautifulSoup
+
+    # Setup
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    driver.get('https://www.yellowpages.com.au/')
+
+    # Bot Time
+    search = driver.find_element_by_id("clue")
+    search_local = driver.find_element_by_id("where")
+    search.send_keys(company)
+    search_local.send_keys(location)
+    search.send_keys(Keys.RETURN)
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    button = soup.find('div', class_='Box__Div-sc-dws99b-0 cINsGc').find('button', class_='MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-fullWidth')
+    print(button.get_text())
+
+    driver.close()
+
+
 def merge(list1, list2, list3):
     merged_list = []
     for i in range(max((len(list1), len(list2), len(list3)))):
@@ -54,7 +83,9 @@ for page in range(1, 10):
         except:
             pass
 
+for companies in company:
+    print(yellow_pages(companies, 'Maryborough'))
+
 
 merged_jobs = merge(company, local, title)
 df = pd.DataFrame(merged_jobs, columns=['Company', 'Location', 'Title'])
-
